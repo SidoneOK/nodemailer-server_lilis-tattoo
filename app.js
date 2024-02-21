262548d54f64675eb4f4ad7e75fc536b25060adf
Oleksii <sidone666@gmail.com>
2024-02-21 13:55:43 +0200

    clone Yuris server

---
+++ app.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://yuratattoodo.com', // use your actual domain name (or localhost), using * is not recommended
+    // optionsSuccessStatus: 200,
+}))
+app.use(express.json({ limit: "150mb" }));
+
+
+app.get('/',  (req, res) => {
+    res.send('HI server is working 2')
+})
+
+
+function requestBodyObject(body) {
+    return {
+      ...body,
+      tattooImgArray: body.tattooImgArray.map((file) => {
+        return { ...file, url: `file length: ${file.url.length}` };
+      }),
+    };
+  }
+  
+  app.post("/sent", function (request, result) {
+    console.log("Request start info", requestBodyObject(request.body));
+  
+    let output = `
+    <p>You have a new sign up for tattooing</p>
+    <h3>Sign Up Profile</h3>
+    <ul>
+      <li>Name: ${request.body.firstName} ${request.body.lastName} </li>
+      <li>Email: ${request.body.email} </li>
+      <li>Phone: ${request.body.telephone} </li>
+      <li>Instagram nickname: ${request.body.instagramNikname} </li>
+      <li>${request.body.firstName} birthday: ${request.body.birthDate} </li>
+      <li>${request.body.firstName} location: ${request.body.location}</li>
+      <li>Description of what ${request.body.firstName} want: ${request.body.message}</li>
+      <li>Tattoo size: ${request.body.TattooSize}</li>
+      <li>Placement on body: ${request.body.Placement}</li>
+      <li>Preferred skin tone: ${request.body.skinTone}</li>
+      <li>Preferred tattoo color: ${request.body.tatooColor}</li>
+      <li>${request.body.firstName} availability: ${request.body.availability}</li>
+      <li>Other inquiries: ${request.body.otherInquires}</li>
+      <li>Contraindications: ${request.body.Contraindications}</li>
+      <li>Limit budget: ${request.body.budget}</li>
+      <li>Person is over 18: ${request.body.age}</li>
+      <li>Will check Spam folder: ${request.body.checkSpam}</li>
+  
+    </ul>
+    `;
+  
+    let transporter = nodemailer.createTransport({
+      servise: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "liliia.test.email@gmail.com",
+        pass: "gpjsjyswzcgpfnch",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  
+    // send mail with defined transport object
+    let mailOptions = {
+      from: "liliia.test.email@gmail.com",
+      to: "yuratattoodo@gmail.com",
+      replyTo: `${request.body.email}`, 
+      subject: `New Sign Up for Tattooing from ${request.body.firstName} ${request.body.lastName}`,
+      text: "profile below",
+      html: output,
+      attachments: request.body.tattooImgArray.map((file) => {
+        return {
+          filename: file.title,
+          content: file.url.split("base64,")[1],
+          encoding: "base64",
+        };
+      }),
+    };
+  
+    let sendMailResultHandler = (error, info) => {
+      if (error) {
+        let errorInfo = {
+          text: "ERROR",
+          message: error,
+          profile: request.body,
+          info: info,
+        };
+        result.json(errorInfo);
+        return;
+      }
+  
+      let emailSent = {
+        text: "Email sent",
+        profile: requestBodyObject(request.body),
+        info: info,
+      };
+      // console.log("Email sent piece", request.body);
+      result.json(emailSent);
+    };
+  
+    transporter.sendMail(mailOptions, sendMailResultHandler);
+  });
+
+
+
+
+app.listen(3000, () => {
+  console.log("server started...");
+});
