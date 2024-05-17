7f8087880f6410afaee8718e862ebdbc29f78abf
Oleksii <sidone666@gmail.com>
2024-05-17 22:20:28 +0300

    fix tg errors and add more consoles for chekings

--- utils/getTgMessade.js
+++ utils/getTgMessade.js
@@ -1,25 +1,49 @@
-module.exports =  function getTgMessade(emailSent) {
+module.exports = function getTgMessade(emailSent) {
+    const escapeMarkdown = (text) => {
+        if (!text) return ''
+        const replacements = {
+            _: '\\_',
+            '*': '\\*',
+            '[': '\\[',
+            ']': '\\]',
+            '(': '\\(',
+            ')': '\\)',
+            '~': '\\~',
+            '`': '\\`',
+            '>': '\\>',
+            '#': '\\#',
+            '+': '\\+',
+            '-': '\\-',
+            '=': '\\=',
+            '|': '\\|',
+            '{': '\\{',
+            '}': '\\}',
+            '.': '\\.',
+            '!': '\\!',
+        }
+        return text.replace(/[_*[\]()~`>#\+\-=|{}.!]/g, (match) => replacements[match])
+    }
+
     const tgMessage = `
-    Email sent successfully:
-    *Name:* ${emailSent.profile.firstName} ${emailSent.profile.lastName}
-    *Email:* ${emailSent.profile.email}
-    *Telephone:* ${emailSent.profile.telephone}
-    *Instagram Nickname:* ${emailSent.profile.instagramNikname}
-    *Birth Date:* ${emailSent.profile.birthDate}
-    *Location:* ${emailSent.profile.location}
-    *Tattoo Size:* ${emailSent.profile.TattooSize}
-    *Placement:* ${emailSent.profile.Placement}
-    *Skin Tone:* ${emailSent.profile.skinTone}
-    *Message:* ${emailSent.profile.message}
-    *Tattoo Color:* ${emailSent.profile.tatooColor}
-    *Availability:* ${emailSent.profile.availability}
-    *Contraindications:* ${emailSent.profile.Contraindications}
-    *Best Days:* ${emailSent.profile.BestDays}
-    *Other Inquiries:* ${emailSent.profile.otherInquires}
-    *Budget:* ${emailSent.profile.budget}
-    *Age:* ${emailSent.profile.age}
-    *Check Spam Folder:* ${emailSent.profile.checkSpam}
-  `;
-    return tgMessage.replace('`', '')
-    // подивитись які символи телеграм не приймає
-  }
+Email sent successfully:
+*Name:* ${escapeMarkdown(emailSent.profile.firstName)} ${escapeMarkdown(emailSent.profile.lastName)}
+*Email:* ${escapeMarkdown(emailSent.profile.email)}
+*Telephone:* ${escapeMarkdown(emailSent.profile.telephone)}
+*Instagram Nickname:* ${escapeMarkdown(emailSent.profile.instagramNikname)}
+*Birth Date:* ${escapeMarkdown(emailSent.profile.birthDate)}
+*Location:* ${escapeMarkdown(emailSent.profile.location)}
+*Tattoo Size:* ${escapeMarkdown(emailSent.profile.TattooSize)}
+*Placement:* ${escapeMarkdown(emailSent.profile.Placement)}
+*Skin Tone:* ${escapeMarkdown(emailSent.profile.skinTone)}
+*Message:* ${escapeMarkdown(emailSent.profile.message)}
+*Tattoo Color:* ${escapeMarkdown(emailSent.profile.tatooColor)}
+*Availability:* ${escapeMarkdown(emailSent.profile.availability)}
+*Contraindications:* ${escapeMarkdown(emailSent.profile.Contraindications)}
+*Best Days:* ${escapeMarkdown(emailSent.profile.BestDays)}
+*Other Inquiries:* ${escapeMarkdown(emailSent.profile.otherInquires)}
+*Budget:* ${escapeMarkdown(emailSent.profile.budget)}
+*Age:* ${escapeMarkdown(emailSent.profile.age)}
+*Check Spam Folder:* ${escapeMarkdown(emailSent.profile.checkSpam)}
+`
+    return tgMessage
+}
--- utils/sendToTg.js
+++ utils/sendToTg.js
@@ -1,28 +1,33 @@
-const axios = require('axios');
+const axios = require('axios')
 
 module.exports = async function sendToTg(message) {
-  try {
-    let url = 'https://api.telegram.org/bot' + process.env.TELEGRAM_BOT_TOKEN + '/sendMessage';
-    let body = {
-      chat_id: process.env.TG_CHAT,
-      parse_mode: 'Markdown',
-      text: message,
-    };
-    const res = await axios.post(url, body, {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        'Access-Control-Allow-Headers':
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-      },
-    });
-    // console.log("No problem", message, res);
-    return res;
-  } catch (error) {
-    console.log(message);
-    console.error('Error sending message to Telegram:', error);
-    throw error;
-  }
-};
+    try {
+        let url = 'https://api.telegram.org/bot' + process.env.TELEGRAM_BOT_TOKEN + '/sendMessage'
+        let body = {
+            chat_id: process.env.TG_CHAT,
+            parse_mode: 'MarkdownV2',
+            text: message,
+        }
+        console.log('Sending request to Telegram:', JSON.stringify(body, null, 2))
+
+        const res = await axios.post(url, body, {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                'Access-Control-Allow-Headers':
+                    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+        })
+        console.log('Response from Telegram:', res.data)
+        return res
+    } catch (error) {
+        if (error.response) {
+            console.error('Error response data:', error.response.data)
+        } else {
+            console.error('Error message:', error.message)
+        }
+        throw error
+    }
+}
